16d8bd974f28b979b44bd3c294d21651e874ac45
AyushK5ingh <146541047+AyushK5ingh@users.noreply.github.com>
2024-12-30 21:29:37 +0000

    restart bug

---
+++ 01-basics/04-operation.js
@@ -0,0 +1,28 @@
+let value = 23
+let negValue = (-value)
+console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(22/2)
+// console.log(22%2)
+
+let str1= "hello "
+let str2=" ayush"
+
+let str3= str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+//good for knowledge but not good practice
+
+console.log(+true)
+console.log(+"")
+
+//link to study documentation
+//https://tc39.es/ecma262/2023/multipage/abstract-operations.html#sec-type-conversion
