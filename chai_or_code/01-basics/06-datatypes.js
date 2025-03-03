adcd398ee8f8436e40d5ddd19c5eca31571bbc2b
Ayush Kumar Singh <ayushkumarm.singh@gmail.com>
2025-03-03 20:12:24 +0000

    dt

--- chai_or_code/01-basics/06-datatypes.js
+++ chai_or_code/01-basics/06-datatypes.js
@@ -12,6 +12,13 @@ const score =100;
 // const score1:number = 100
 //only typesccript
 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
 const id=Symbol('123');
 const anotherId=Symbol('123');
 console.log (id==anotherId);
